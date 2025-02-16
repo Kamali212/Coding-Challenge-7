a2e8acde161b381561a9ef3c5a02151da6f026ee
Kamali Hicks <kamalikh3@gmail.com>
2025-02-15 20:53:09 -0500

    Task 3 - Customer Loyalty Discount

--- cc_7.js
+++ cc_7.js
@@ -17,3 +17,22 @@ let calculateHourlyWage = function(salary, hoursPerWeek) // Use a function expre
 
 console.log(calculateHourlyWage(52000, 40)); // Expected output: "Hourly Wage: $25.00"
 console.log(calculateHourlyWage(75000, 35)); // Expected output: "Hourly Wage: $41.21"
+
+// Task 3 Arrow Function
+
+function calculateLoyaltyDiscount(amount, years) { // Function to calculate loyalty discount
+    let discount = 0;
+    if (years >= 5) { // If years are greater than or equal to 5, discount is 15%
+        discount = 0.15; 
+    } else if (years >= 3) { // If years are greater than or equal to 3, discount is 10%
+        discount = 0.10;
+    } else { // If years are less than 3 discount is 5%
+        discount = 0.05;
+    }
+
+    let discountedPrice = amount - (amount * discount); // Calculating discounted price
+    return "Discounted Price: $" + discountedPrice.toFixed(2); // Returns value to two decimal points
+}
+
+console.log(calculateLoyaltyDiscount(100, 6)); // Expected output: "Discounted Price: $85.00"
+console.log(calculateLoyaltyDiscount(200, 2)); // Expected output: "Discounted Price: $190.00"
